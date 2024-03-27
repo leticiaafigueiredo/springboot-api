dcfdcee91022597b4789d3493cc51280a33c8024
Leticia Figueiredo <143857559+leticiaafigueiredo@users.noreply.github.com>
2024-03-27 14:44:34 -0300

    mudando nome do pacote e criando os repositorios

--- src/main/java/com/leticiafigueiredo/demo/models/User.java
+++ src/main/java/com/leticiafigueiredo/demo/models/User.java
@@ -1,4 +1,4 @@
-package com.example.demo.models;
+package com.leticiafigueiredo.demo.models;
 
 import java.util.ArrayList;
 import java.util.List;
---
+++ src/main/java/com/leticiafigueiredo/demo/repositories/UserRepository.java
@@ -0,0 +1,18 @@
+package com.leticiafigueiredo.demo.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.leticiafigueiredo.demo.models.User;
+import java.util.List;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> { //conecta com a classe User que tem o id tipo Long
+    
+    //esse metodo tem varias funcoes para busca na tabela como:
+    // - findById(List<User> findById(Long id);
+    // - fingByPassword
+    // - findByTask
+    // ...
+}
