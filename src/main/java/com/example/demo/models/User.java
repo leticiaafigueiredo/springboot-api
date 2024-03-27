1233a0244a63d8fa26eda15d186215c78095ac60
Leticia Figueiredo <143857559+leticiaafigueiredo@users.noreply.github.com>
2024-03-27 10:27:10 -0300

    criando tabela de tarefas do usuario e fazendo o relacionamento entre eles

--- src/main/java/com/example/demo/models/User.java
+++ src/main/java/com/example/demo/models/User.java
@@ -1,17 +1,19 @@
 package com.example.demo.models;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import java.util.Objects;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonProperty.Access;
@@ -46,7 +48,9 @@ public class User {
     @Size(groups = { CreateUser.class, UpdateUser.class }, min = 8, max = 60)
     private String password;
 
-    // private List<Task> tasks = new ArrayList<Task>();
+
+    @OneToMany(mappedBy = "user") //1 ususario tem varias tasks
+    private List<Task> tasks = new ArrayList<Task>();
 
     public User() {
     }
@@ -79,6 +83,18 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }    
+    
+    public static String getTableName() {
+        return TABLE_NAME;
+    }
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
     }
 
     @Override
@@ -106,5 +122,7 @@ public class User {
         result = prime * result + ((this.id == null) ? 0 : this.id.hashCode());
         return result;
     }
+
+
 
 }
