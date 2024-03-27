521a7e37fb9ebea7231bef65d2a6a8419d3ebaa3
Leticia Figueiredo <143857559+leticiaafigueiredo@users.noreply.github.com>
2024-03-27 15:42:59 -0300

    modelando CRUD de Task e refatorando O CRUD de User = a criação de tarefas só será permitida após a criação do usuário, de forma sequencial e não paralela

--- src/main/java/com/leticiafigueiredo/demo/services/UserService.java
+++ src/main/java/com/leticiafigueiredo/demo/services/UserService.java
@@ -8,16 +8,12 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.leticiafigueiredo.demo.repositories.UserRepository;
 import com.leticiafigueiredo.demo.models.User;
-import com.leticiafigueiredo.demo.repositories.TaskRepository;
 
 @Service
 public class UserService {
 
     @Autowired
     private UserRepository userRepository;
-
-    @Autowired
-    private TaskRepository taskRepository;
 
     public User findById(Long id) {
         Optional<User> user = this.userRepository.findById(id);
@@ -29,7 +25,6 @@ public class UserService {
     public User create(User obj) {
         obj.setId(null);
         obj = this.userRepository.save(obj);
-        this.taskRepository.saveAll(obj.getTasks());
         return obj;
     }
 
