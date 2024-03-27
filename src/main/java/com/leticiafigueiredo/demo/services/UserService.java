bd2b6674e2e2df646d6f86c0f0c093bb98a0ac71
Leticia Figueiredo <143857559+leticiaafigueiredo@users.noreply.github.com>
2024-03-27 15:27:03 -0300

    moldando o CRUD do User

---
+++ src/main/java/com/leticiafigueiredo/demo/services/UserService.java
@@ -0,0 +1,52 @@
+package com.leticiafigueiredo.demo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.leticiafigueiredo.demo.repositories.UserRepository;
+import com.leticiafigueiredo.demo.models.User;
+import com.leticiafigueiredo.demo.repositories.TaskRepository;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    public User findById(Long id) {
+        Optional<User> user = this.userRepository.findById(id);
+        return user.orElseThrow(() -> new RuntimeException(
+                "Usuário não encontrado! Id: " + id + ", Tipo: " + User.class.getName()));
+    }
+
+    @Transactional //salvar informacoes no banco
+    public User create(User obj) {
+        obj.setId(null);
+        obj = this.userRepository.save(obj);
+        this.taskRepository.saveAll(obj.getTasks());
+        return obj;
+    }
+
+    @Transactional
+    public User update(User obj) { //so e possivel atualizar a senha, nunca o usuario
+        User newObj = findById(obj.getId());
+        newObj.setPassword(obj.getPassword());
+        return this.userRepository.save(newObj);
+    }
+
+    public void delete(Long id) {
+        findById(id);
+        try {
+            this.userRepository.deleteById(id);
+        } catch (Exception e) {
+            throw new RuntimeException("Não é possível excluir pois há entidades relacionadas!");
+        }
+    }
+
+}
